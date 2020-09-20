2fc13d8bba0cc29c85938e7164b92cd16dc3e44b
Jitender Pal Singh <jitenderpalsingh66@gmail.com>
2020-09-21 03:38:12 +0530

    Initial commit

---
+++ src/components/ButtonComponent.js
@@ -0,0 +1,32 @@
+import React from "react";
+import "../styles.css";
+function ButtonComponent(props) {
+  // function start() {
+  //   props.start();
+  // }
+  return (
+    <div>
+      {props.status === 0 ? (
+        <button className="stopwatch-btn start-btn" onClick={props.start}>
+          Start
+        </button>
+      ) : (
+        ""
+      )}
+      {props.status === 1 ? (
+        <button className="stopwatch-btn pause-btn" onClick={props.pause}>
+          Pause
+        </button>
+      ) : (
+        ""
+      )}
+      <button className="stopwatch-btn reset-btn" onClick={props.reset}>
+        Reset
+      </button>
+      <button className="stopwatch-btn lap-btn" onClick={props.laps}>
+        Lap
+      </button>
+    </div>
+  );
+}
+export default ButtonComponent;
